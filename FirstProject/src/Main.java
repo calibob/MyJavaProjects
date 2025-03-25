54f28e59abeabcc7e73605b86851781522261758
calibob <yassinezairi242@gmail.com>
2025-03-25 14:01:34 +0000

    adding the javaProject

---
+++ FirstProject/src/Main.java
@@ -0,0 +1,22 @@
+
+// variable = a reusable container for a value
+// Primitive variable = simple value stored directly in memory(stack)
+// Reference variable = memory adress (stack) that points to the (heap)
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Toujours dans ma tete j'ai gare mon RS la\n");
+       /* int age = 21;
+        System.out.println(age);
+        System.out.println("The age is " + age);
+        */
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = 12.5;
+        System.out.println("$"+ price);
+
+    }
+
+}
